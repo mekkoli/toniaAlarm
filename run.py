fd5d6b165d9caa1dcfb2640bfe7d544128214fd9
marco.mescoli <marco.mescoli@fermi.mo.it>
2019-04-22 18:49:29 +0200

    before user/passw complete report

--- run.py
+++ run.py
@@ -1,35 +1,82 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 import datetime
 import platform
 
 app = Flask(__name__)
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'azzarola'
+credentials = {'tonia':'Alarm'}
 
 @app.template_filter("datetimefilter")
 def datetimefilter(value, format='%Y-%m-%d %02H:%02M:%02S'):
   """Convert a datetime to a differentformat."""
   return value.strftime(format)
 
-@app.route("/")
-def login():
-  return render_template(
-    'login.html',
-    currentTime = datetime.datetime.now(),
-    annoScolas = "18/19",
-    title = "tonia alarm web configuration"
-  )
-
-@app.route("/home", methods=['POST', 'GET'])
+@app.route("/home")
 def home():
-  if request.method == 'POST':
-    username = request.form['username'],
-    password = request.form['password']
-  else:
-    username = ("no user", "none")
   return render_template(
     'sounds.html',
     currentTime = datetime.datetime.now(),
+    annoScolas = "18/19",
     title = "tonia alarm web configuration",
-    user = username[0]
+    user = session['username']
+  )
+
+@app.route("/", methods=['POST', 'GET'])
+def login():
+  if session['logged_in'] == False:
+    nextPage = "login.html"
+    user = ""
+    message = ""
+    if request.method == 'POST':
+      user = request.form['username']
+      pw  = request.form['password']
+      if user in credentials:
+        if credentials[user] == pw:
+          session['username'] = user
+          session['logged_in'] = True
+          nextPage = 'sounds.html'
+          message = ""
+        else:
+          session['username'] = None
+          session['logged_in'] = False
+          nextPage = 'login.html'
+          user = ""
+          message = "wrong pw"
+      else:
+        session['username'] = None
+        session['logged_in'] = False
+        nextPage = 'login.html'
+        user = ""
+        if user != "":
+          message = "wrong user"
+    else:
+      session['username'] = None
+      session['logged_in'] = False
+      nextPage = 'login.html'
+      user = ""
+      message = "get method"
+    return render_template(
+      nextPage,
+      user = user,
+      currentTime = datetime.datetime.now(),
+      annoScolas = "18/19",
+      title = "tonia alarm web configuration",
+      message = message
+    )
+  else:
+    return redirect(url_for('home'))
+  
+@app.route("/logout")
+def logout():
+  session['logged_in'] = False
+  session['username'] = None
+  return render_template(
+    "login.html",
+    currentTime = datetime.datetime.now(),
+    title = "tonia alarm web configuration",
+    user = "",
+    message = ""
   )
 
 @app.route("/getAlarmStatus")
@@ -37,7 +84,8 @@ def getAlarmStatus():
   return render_template(
     'status.html',
     currentTime = datetime.datetime.now(),
-    title = "tonia alarm status"
+    title = "tonia alarm status",
+    user = session['username']
   )
 
 @app.route('/halt')
